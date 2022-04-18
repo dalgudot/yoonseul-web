e7fa5c15d97464d578c6e5dcde7d751684609000
dalgudot <dalgudot@gmail.com>
2022-04-18 12:08:56 +0900

    update pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
 import type { NextPage } from 'next';
+import style from '../styles/Home.module.css';
+import { useRouter } from 'next/router';
 
 const Home: NextPage = () => {
-  return <>윤슬 지원</>;
+  const router = useRouter();
+  const locale = router.locale;
+
+  return (
+    <main className={style.main}>
+      {locale === 'ko' ? '지금을 기억하기' : 'Remember this moment'}
+    </main>
+  );
 };
 
 export default Home;
--- src/pages/privacy.tsx
+++ src/pages/privacy.tsx
@@ -1,7 +1,17 @@
 import type { NextPage } from 'next';
+import style from '../styles/Home.module.css';
+import { useRouter } from 'next/router';
 
 const Privacy: NextPage = () => {
-  return <>개인 정보</>;
+  const router = useRouter();
+  const locale = router.locale;
+
+  const korean = '윤슬은 어떠한 데이터도 수집하지 않습니다.';
+  const english = 'YoonSeul does not collect any data from this app.';
+
+  return (
+    <main className={style.main}>{locale === 'ko' ? korean : english}</main>
+  );
 };
 
 export default Privacy;
